d66f5320c636b788cfe79568966f0a1b54fd3850
Ahmad Arsalan Zahid <ahmad996.arsalan@gmail.com>
2026-04-30 13:18:31 +0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,16 @@
-import { Sequelize } from 'sequelize';
+import 'dotenv/config';
 
 if (!process.env.MYSQL_URL) {
   throw new Error('MYSQL_URL is not defined');
 }
 
-export const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  dialect: 'mysql',
-  logging: false,
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+export default {
+  development: {
+    url: process.env.MYSQL_URL,
+    dialect: 'mysql',
   },
-});
-
-export async function connectDB() {
-  await sequelize.authenticate();
-  console.log('Database connected');
-}
+  production: {
+    url: process.env.MYSQL_URL,
+    dialect: 'mysql',
+  },
+};
